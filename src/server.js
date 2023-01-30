f4a0e9ee7b13a78cafc57df35a5308218fb645c4
Arthur Moreira de Deus <arthurdedeus@gmail.com>
2023-01-30 11:49:08 -0300

    saving users in memory

--- src/server.js
+++ src/server.js
@@ -1,14 +1,26 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('GET /users')
+    return res
+    .setHeader('Content-Type', 'application/json')
+    .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
-    return res.end('POST /users')
+    const user = {
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+    }
+    users.push(user)
+    return res
+    .setHeader('Content-Type', 'application/json')
+    .end(JSON.stringify(user))
   }
 
   return res.end('Hello World')
