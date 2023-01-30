c7fa3134358c6131ef4c0d0a4f566d408b22e47c
Arthur Moreira de Deus <arthurdedeus@gmail.com>
2023-01-30 11:31:42 -0300

    routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
-import http from 'node:http';
+import http from 'node:http'
 
-const server  = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('GET /users')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('POST /users')
+  }
+
   return res.end('Hello World')
 })
 
