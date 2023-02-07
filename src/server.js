f5037c663b0d0b588c5a5d578b0976d2d41c236f
Arthur Moreira de Deus <arthurdedeus@gmail.com>
2023-02-07 20:04:28 -0300

    middleware

--- src/server.js
+++ src/server.js
@@ -1,27 +1,21 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-    .setHeader('Content-Type', 'application/json')
-    .end(JSON.stringify(users))
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
-    const user = {
-      id: 1,
-      name: 'John Doe',
-      email: 'johndoe@email.com',
-    }
-    users.push(user)
-    return res
-    .setHeader('Content-Type', 'application/json')
-    .writeHead(201)
-    .end(JSON.stringify(user))
+    const { name, email } = req.body
+    const id = users.length + 1
+    users.push({ id, name, email })
+    return res.writeHead(201).end(JSON.stringify(users.find((user) => user.id === id)))
   }
 
   return res.writeHead(404).end()
