c2cb786f143893c4c8a5286e934ed5f87f271d6b
Arthur Moreira de Deus <arthurdedeus@gmail.com>
2023-02-07 20:23:17 -0300

    database

--- src/server.js
+++ src/server.js
@@ -1,21 +1,24 @@
+import { Database } from './database.js'
 import http from 'node:http'
 import { json } from './middlewares/json.js'
+import { randomUUID } from 'node:crypto'
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
   await json(req, res)
 
   if (method === 'GET' && url === '/users') {
+    const users = database.select('users')
     return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
-    const id = users.length + 1
-    users.push({ id, name, email })
-    return res.writeHead(201).end(JSON.stringify(users.find((user) => user.id === id)))
+    const userData = { id: randomUUID(), name, email }
+    const user = database.insert('users', userData)
+    return res.writeHead(201).end(JSON.stringify(user))
   }
 
   return res.writeHead(404).end()
